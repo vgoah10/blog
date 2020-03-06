0085f11a48f6f931d44d956cb274b3d691a6d5ae
Li Fan <fan.li@hlyt.com>
2020-03-06 02:47:06 +0000

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -976,7 +976,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "nikolademo"
+COMMENT_SYSTEM_ID = "vgoah10@yeah.net"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
